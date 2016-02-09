e9f2568dc20f15e1c6131c62b16371d898fd1b23
charlesdrews <charlesdrews@gmail.com>
2016-02-08 20:22:02 -0500

    Add unit tests for Place class

--- NeighborhoodGuide/app/src/test/java/com/charlesdrews/neighborhoodguide/PlaceTest.java
+++ NeighborhoodGuide/app/src/test/java/com/charlesdrews/neighborhoodguide/PlaceTest.java
@@ -9,5 +9,78 @@ import com.charlesdrews.neighborhoodguide.places.Place;
  * Created by charlie on 2/4/16.
  */
 public class PlaceTest {
-    //TODO - add tests
+    private Place place = new Place(
+            5,
+            "Grand Central Terminal",
+            "42nd & Lex",
+            "Midtown",
+            "Public Transportation",
+            "train station",
+            true,
+            3.5f,
+            "look at the stars on the ceiling"
+    );
+
+    @Test
+    public void testGetId() {
+        assertEquals(5, place.getId());
+    }
+
+    @Test
+    public void testGetTitle() {
+        assertEquals("Grand Central Terminal", place.getTitle());
+    }
+
+    @Test
+    public void testGetLocation() {
+        assertEquals("42nd & Lex", place.getLocation());
+    }
+
+    @Test
+    public void testGetNeighborhood() {
+        assertEquals("Midtown", place.getNeighborhood());
+    }
+
+    @Test
+    public void testGetCategory() {
+        assertEquals("Public Transportation", place.getCategory());
+    }
+
+    @Test
+    public void testGetDescription() {
+        assertEquals("train station", place.getDescription());
+    }
+
+    @Test
+    public void testIsFavorite() {
+        assertEquals(true, place.isFavorite());
+    }
+
+    @Test
+    public void testSetFavoriteStatus() {
+        place.setFavoriteStatus(false);
+        assertEquals(false, place.isFavorite());
+    }
+
+    @Test
+    public void testGetRating() {
+        assertEquals(3.5f, place.getRating(), 0.0f);
+    }
+
+    @Test
+    public void testSetRating() {
+        place.setRating(4.0f);
+        assertEquals(4.0f, place.getRating(), 0.0f);
+    }
+
+    @Test
+    public void textGetNote() {
+        assertEquals("look at the stars on the ceiling", place.getNote());
+    }
+
+    @Test
+    public void testSetNote() {
+        place.setNote("hello");
+        assertEquals("hello", place.getNote());
+    }
 }
